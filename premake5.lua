42c4e3bb25ecb18b33fab37933af738db5a6ab0d
Porl <p.j.riding.91@gmail.com>
2024-07-28 19:22:20 +0100

    Move zlib into separate project.

--- premake5.lua
+++ premake5.lua
@@ -1,137 +1,77 @@
+project "zlib"
+	kind "StaticLib"
+	language "C"
+	warnings "off"
+
+	targetdir "bin/%{cfg.buildcfg}"
+	objdir "obj/%{cfg.buildcfg}"
+
+	files {
+		"contrib/zlib/*.c",
+		"contrib/zlib/*.h"
+	}
+
+	filter "platforms:x86"
+		architecture "x86"
+
+	filter "platforms:x64"
+		architecture "x64"
+		excludes {
+			"contrib/zlib/inffas86.c"
+		}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
 project "Assimp"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
-	staticruntime "off"
-
-	targetdir ("bin/%{cfg.buildcfg}")
-	objdir ("obj/%{cfg.buildcfg}")
+	warnings "off"
+	
+	targetdir "bin/%{cfg.buildcfg}"
+	objdir "obj/%{cfg.buildcfg}"
 
 	files {
-		-- Dependencies
-		"assimp/contrib/unzip/**",
-		"assimp/contrib/irrXML/**",
-		"assimp/contrib/zlib/*",
-		-- Common
-		"assimp/code/Common/**",
-		"assimp/code/PostProcessing/**",
-		"assimp/code/Material/**",
-		"assimp/code/CApi/**",
-		"assimp/code/Geometry/**",
-		-- Importers
-		"assimp/code/AssetLib/IQM/**",
-		"assimp/code/AssetLib/Assbin/**",
+		"*.cpp",
+		"*.h",
 
-		"assimp/code/AssetLib/Collada/**",
-		"assimp/code/AssetLib/Obj/**",
-		-- "assimp/code/AssetLib/Blender/**", "assimp/contrib/poly2tri/poly2tri/**",
-		"assimp/code/AssetLib/FBX/**",
-		-- "assimp/code/glTF2/**",
-		-- "assimp/code/glTF/**",
-		"assimp/code/Assbin/**" -- Very fast format to parse/write, useful for caching
+		"contrib/irrXML/*.cpp",
+		"contrib/irrXML/*.h",
+		"contrib/unzip/*.c",
+		"contrib/unzip/*.h",
+		
+		"code/*.cpp",
+		
+		"include/*.h",
 	}
 
 	includedirs {
-		"_config_headers/",
-		"_config_headers/assimp/",
-		"assimp/",
-		"assimp/contrib/",
-		"assimp/contrib/irrXML/",
-		"assimp/contrib/unzip/",
-		"assimp/contrib/rapidjson/include/",
-		"assimp/contrib/pugixml/src/",
-		"assimp/contrib/zlib/",
-		"assimp/contrib/utf8cpp/source",
-		"assimp/code",
-		"assimp/include",
+		"contrib/irrXML",
+		"contrib/unzip",
+		"contrib/zlib",
+
+		"include"
 	}
 
-	defines {
-		"ASSIMP_BUILD_NO_3D_IMPORTER",
-		"ASSIMP_BUILD_NO_3DS_IMPORTER",
-		"ASSIMP_BUILD_NO_3MF_IMPORTER",
-		"ASSIMP_BUILD_NO_AC_IMPORTER",
-		"ASSIMP_BUILD_NO_AMF_IMPORTER",
-		"ASSIMP_BUILD_NO_ASE_IMPORTER",
-		-- "ASSIMP_BUILD_NO_ASSBIN_IMPORTER"
-		"ASSIMP_BUILD_NO_B3D_IMPORTER",
-		"ASSIMP_BUILD_NO_BLEND_IMPORTER",
-		"ASSIMP_BUILD_NO_BVH_IMPORTER",
-		"ASSIMP_BUILD_NO_C4D_IMPORTER",
-		"ASSIMP_BUILD_NO_COB_IMPORTER",
-		-- "ASSIMP_BUILD_NO_COLLADA_IMPORTER",
-		"ASSIMP_BUILD_NO_CSM_IMPORTER",
-		"ASSIMP_BUILD_NO_DXF_IMPORTER",
-		-- "ASSIMP_BUILD_NO_FBX_IMPORTER",
-		"ASSIMP_BUILD_NO_GLTF_IMPORTER",
-		"ASSIMP_BUILD_NO_HMP_IMPORTER",
-		"ASSIMP_BUILD_NO_IFC_IMPORTER",
-		"ASSIMP_BUILD_NO_IRR_IMPORTER",
-		"ASSIMP_BUILD_NO_IRRMESH_IMPORTER",
-		"ASSIMP_BUILD_NO_LWO_IMPORTER",
-		"ASSIMP_BUILD_NO_LWS_IMPORTER",
-		"ASSIMP_BUILD_NO_M3D_IMPORTER",
-		"ASSIMP_BUILD_NO_MD2_IMPORTER",
-		"ASSIMP_BUILD_NO_MD3_IMPORTER",
-		"ASSIMP_BUILD_NO_MD5_IMPORTER",
-		"ASSIMP_BUILD_NO_MDC_IMPORTER",
-		"ASSIMP_BUILD_NO_MDL_IMPORTER",
-		"ASSIMP_BUILD_NO_MMD_IMPORTER",
-		"ASSIMP_BUILD_NO_MS3D_IMPORTER",
-		"ASSIMP_BUILD_NO_NDO_IMPORTER",
-		"ASSIMP_BUILD_NO_NFF_IMPORTER",
-		-- "ASSIMP_BUILD_NO_OBJ_IMPORTER",
-		"ASSIMP_BUILD_NO_OFF_IMPORTER",
-		"ASSIMP_BUILD_NO_OGRE_IMPORTER",
-		"ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
-		"ASSIMP_BUILD_NO_PLY_IMPORTER",
-		"ASSIMP_BUILD_NO_Q3BSP_IMPORTER",
-		"ASSIMP_BUILD_NO_Q3D_IMPORTER",
-		"ASSIMP_BUILD_NO_RAW_IMPORTER",
-		"ASSIMP_BUILD_NO_SIB_IMPORTER",
-		"ASSIMP_BUILD_NO_SMD_IMPORTER",
-		"ASSIMP_BUILD_NO_STEP_IMPORTER",
-		"ASSIMP_BUILD_NO_STL_IMPORTER",
-		"ASSIMP_BUILD_NO_TERRAGEN_IMPORTER",
-		"ASSIMP_BUILD_NO_X_IMPORTER",
-		"ASSIMP_BUILD_NO_X3D_IMPORTER",
-		"ASSIMP_BUILD_NO_XGL_IMPORTER"
-	}
+	links { "zlib" }
 
-	defines {
-		"ASSIMP_BUILD_NO_COLLADA_EXPORTER",
-		"ASSIMP_BUILD_NO_X_EXPORTER",
-		"ASSIMP_BUILD_NO_STEP_EXPORTER",
-		"ASSIMP_BUILD_NO_OBJ_EXPORTER",
-		"ASSIMP_BUILD_NO_STL_EXPORTER",
-		"ASSIMP_BUILD_NO_PLY_EXPORTER",
-		"ASSIMP_BUILD_NO_3DS_EXPORTER",
-		"ASSIMP_BUILD_NO_GLTF_EXPORTER",
-		-- "ASSIMP_BUILD_NO_ASSBIN_EXPORTER",
-		"ASSIMP_BUILD_NO_ASSXML_EXPORTER",
-		"ASSIMP_BUILD_NO_X3D_EXPORTER",
-		"ASSIMP_BUILD_NO_FBX_EXPORTER",
-		"ASSIMP_BUILD_NO_M3D_EXPORTER",
-		"ASSIMP_BUILD_NO_3MF_EXPORTER",
-		"ASSIMP_BUILD_NO_ASSJSON_EXPORTER"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
+	filter "platforms:x86"
+		architecture "x86"
+		defines { "_M_IX86" }
+		
+	filter "platforms:x64"
+		architecture "x64"
+		defines { "_M_X64" }
 
 	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+		defines { "DEBUG" }
+		symbols "On"
 
 	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+		defines { "NDEBUG" }
+		optimize "On"
