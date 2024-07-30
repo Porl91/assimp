d1a346f791ae21b5a29a3db1d431764a16b8e7e9
Porl <p.j.riding.91@gmail.com>
2024-07-30 20:37:52 +0100

    Hard-code revision.h. Don't need to include the main repo GH release versions.

---
+++ include/assimp/revision.h
@@ -0,0 +1,12 @@
+#ifndef ASSIMP_REVISION_H_INC
+#define ASSIMP_REVISION_H_INC
+
+#define GitVersion 0x0
+#define GitBranch "master"
+
+#define VER_MAJOR 1
+#define VER_MINOR 0
+#define VER_PATCH 0
+#define VER_BUILD 0
+
+#endif // ASSIMP_REVISION_H_INC
